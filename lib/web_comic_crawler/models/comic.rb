d0f9c7c8d75a4d361e5c5701f2438cecc551fee7
yuta-muramoto <s1513114@u.tsukuba.ac.jp>
2016-07-10 13:27:12 +0900

    add accessor

--- lib/web_comic_crawler/models/comic.rb
+++ lib/web_comic_crawler/models/comic.rb
@@ -1,6 +1,16 @@
 module WebComicCrawler
   module Models
     class Comic
+      attr_accessor :title, :url, :author, :image_url, :description
+
+      def initialize(params)
+        @url         = params[:url]
+        @title       = params[:title]
+        @author      = params[:author]
+        @image_url   = params[:image_url]
+        @description = params[:description]
+      end
+
       def self.create(params)
         params[:title]
       end
