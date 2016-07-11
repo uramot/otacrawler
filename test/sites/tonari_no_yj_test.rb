883627fbfe53f243c004462591dbeb461fac9b2d
yuta-muramoto <s1513114@u.tsukuba.ac.jp>
2016-07-11 13:01:33 +0900

    refactoring comic test

--- test/sites/tonari_no_yj_test.rb
+++ test/sites/tonari_no_yj_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class TonariNoYJTest < Minitest::Test
   def setup
     @yj = Otacrawler::Sites::TonariNoYJ.new
+    @url = "http://www.tonarinoyj.jp/manga/shabettemoneko/"
+    @comic = Otacrawler::Models::Comic.find_by(url: @url)
   end
 
   def test_analyze
@@ -10,33 +12,23 @@ class TonariNoYJTest < Minitest::Test
   end
 
   def test_url_list
-    url = "http://www.tonarinoyj.jp/manga/onepanman/"
-    assert_equal url, @yj.url_list.first
+    assert_equal @comic.url, @yj.url_list.first
   end
 
   def test_title
-    assert_equal "ワンパンマン", @yj.titles.first
+    assert_equal @comic.title, @yj.titles.first
   end
 
   def test_authors
-    assert_equal "原作/ＯＮＥ", @yj.authors[0][0]
+    assert_equal @comic.authors.first.name, @yj.authors[0][0]
   end
 
   def test_image_url
     image_url = "http://www.tonarinoyj.jp/manga/assets/assets_c/2015/04/onepunchman-tn-thumb-180xauto-239.jpg"
-    assert_equal image_url, @yj.image_url_list.first
+    assert_equal @comic.image_url, @yj.image_url_list.first
   end
 
   def test_description
-    url = "http://www.tonarinoyj.jp/manga/onepanman"
-    description = @yj.description(url)
-    expect =<<EOS
-時代が求めたニューヒーロー『一撃男』と言えば?!?\r
-『WEB界のカリスマ』と『最強ジャンプ遺伝子』の超強力タッグが描き出す！ 平熱系最強ヒーロー“サイタマ”の日常ノックアウトコミック!!\r
-\r
-ニコニコ静画「ワンパンマン静止画MADコンテスト」グランプリ受賞作決定！\r
-詳細はこちらから！\r
-EOS
-    assert_equal expect, description
+    assert_equal @comic.description, @yj.description(@url)
   end
 end
